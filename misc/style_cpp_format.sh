54a9479318b8d81cb2fffbd9a67c86876ec49644
frankmejzlik <frankmejzlik@gmail.com>
2020-07-13 21:10:17 +0200

    Minor restructure.

--- misc/style_cpp_format.sh
+++ misc/style_cpp_format.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd `dirname $0` #lol
+echo `dirname $0`
 clang-format -style="{BasedOnStyle: Mozilla, UseTab: ForIndentation, IndentWidth: 8, TabWidth: 8, AccessModifierOffset: -8, PointerAlignment: Right}" -verbose -i ../core/src/*.hpp ../core/src/*.h ../core/src/*.cpp ../core/*.h ../core/*.cpp
