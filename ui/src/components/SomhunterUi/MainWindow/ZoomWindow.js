f0947e9fd38f1bac711d95928a93c9d4d3aef3dd
frankmejzlik <frankmejzlik@gmail.com>
2020-10-05 18:28:34 +0200

    WIP: Implement zoom window.

---
+++ ui/src/components/SomhunterUi/MainWindow/ZoomWindow.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Modal, Button, Container } from "react-bootstrap";
+
+function ReplayWindow(props) {
+  return (
+    <Modal
+      {...props}
+      backdrop={false}
+      enforceFocus={false}
+      keyboard={false}
+      autoFocus={false}
+      animation={false}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className="zoom-window off-canvas-window window"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Zoom
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <img src={props.frame.src} alt={props.frame.id} />
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default ReplayWindow;
