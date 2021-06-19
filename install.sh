a78fe39f9759446780fd39826abe020559d27626
Frank Mejzlik <frankmejzlik@gmail.com>
2021-06-19 11:20:53 +0200

    Fix.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${ABSOLUTE_PATH}
 
 
-BUILD_TYPE=Release
+BUILD_TYPE=RelWithDebInfo
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 if [ -z ${1} ]; then 
