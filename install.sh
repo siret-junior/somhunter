98c7129993a24118ef9d197148546109bb26d34c
Frank Mejzlik <frankmejzlik@gmail.com>
2021-06-19 11:12:28 +0200

    Improve build scripts.

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd ${ABSOLUTE_PATH}
+
+
+BUILD_TYPE=Release
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+if [ -z ${1} ]; then 
+    echo "Build type not specified, using default '${BUILD_TYPE}'..."
+else
+    BUILD_TYPE=${1}
+    echo "Build type is specified to '${BUILD_TYPE}'..."
+fi
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 echo "--------------------------------"
 printf "Installing 'somhunter'...\n"
 
-sh ./somhunter-core/install.sh
+sh ./somhunter-core/install.sh ${BUILD_TYPE}
 sh ./somhunter-data-server/install.sh
 sh ./somhunter-ui/install.sh
 
