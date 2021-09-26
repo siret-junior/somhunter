197fa04dcd1913895059eb7dd8fd5a4879eb9333
Frank Mejzlik <frankmejzlik@gmail.com>
2021-09-26 11:04:41 +0200

    Bump.

--- run-core.sh
+++ run-core.sh
@@ -4,8 +4,8 @@ ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $ABSOLUTE_PATH # CD to script dir
 
 # Use this if you want instant restarts
-supervise ./somhunter-core/
+#supervise ./somhunter-core/
 
 # Normal run
-#bash ./somhunter-core/run.sh
+bash ./somhunter-core/run.sh
 
